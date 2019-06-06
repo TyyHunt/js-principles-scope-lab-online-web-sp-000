4a74343fde32856382e922f6ca6d8f337600d4c6
Tyler Hunt <tyhunt04@gmail.com>
2019-06-06 04:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function overwriteBestCustomer() {
   bestCustomer = 'maybe bob'
 }
 
-function changeLeastFavoriteCustomer(bestcustomer = 'bob') {
-
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob'
 }
